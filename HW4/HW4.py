7d3e8221d76eba3a043acddd4900c3683b457686
John Schiltz <johnschiltz2000@gmail.com>
2022-10-10 14:06:12 -0500

    finished

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -20,10 +20,38 @@ def split_join():
 
 # - Play with all three flavors of the range() function as in slides #49-51 of Part 2.
 def range_stuff():
-  long_boi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+  print("Range 0-8")
+  print(list(range(8)))
+
+  print("Range 0-8 with defined start point")
+  print(list(range(0,8)))
+
+
+  print("Range 0-20 with defined start point and step distance")
+  print(list(range(0,20,3)))
 
 # - Show use of "for" loop iterated over (i) string, (ii) list, and (iii) range(). Take different screenshots if needed.
+def for_stuff():
+  
+  long_boi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+  long_str = "Pneumonoultramicroscopicsilicovolcanoconiosis"
+
+  print("\nreversed str")
+  for i in long_str[::-1]:
+    print(f"{i} ", end="")
+
+  print("\nEvery element")
+  for i in long_boi:
+    print(f"{i} ", end="")
+
+  print("\nEvery other element")
+  for i in range(0,len(long_boi)-1,2):
+    print(f"{long_boi[i]} ", end="")
+
 
 if __name__ == "__main__":
   play_with_lists()
   split_join()
+  range_stuff()
+  for_stuff()
